addbc786c41ec486112f1a76fbe0b39eb82f48eb
Rénich Bon Ćirić <renich@evalinux.com>
2026-08-13 20:50:04 -0600

    test(spec): assert v0.2.0 library version in spec
    Co-authored-by: Antigravity <antigravity@google.com>
    Signed-off-by: Rénich Bon Ćirić <renich@woralelandia.com>

--- spec/crystal-tpm2-tss_spec.cr
+++ spec/crystal-tpm2-tss_spec.cr
@@ -2,6 +2,6 @@ require "./spec_helper"
 
 describe Crystal::Tpm2::Tss do
   it "defines library version" do
-    Crystal::Tpm2::Tss::VERSION.should eq("0.1.0")
+    Crystal::Tpm2::Tss::VERSION.should eq("0.2.0")
   end
 end
